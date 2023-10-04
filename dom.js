4c8d84c5626e6bd227364dfca2187de59a8b26a5
Uma Shankar <umashankarjha1066@gmail.com>
2023-10-05 04:45:59 +0530

    third commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+const headerTitle = document.getElementById("header-title");
+headerTitle.innerText = "Hello World";
+
+const header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+
+const title = document.querySelector(".title");
+title.style.fontWeight = "bold";
+title.style.color = "green";
