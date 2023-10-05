a6fde55c9e0090d0b92d8ac1533c5f92f5deed7b
Uma Shankar <umashankarjha1066@gmail.com>
2023-10-05 23:20:09 +0530

    added

--- dom.js
+++ dom.js
@@ -8,10 +8,17 @@ const title = document.querySelector(".title");
 title.style.fontWeight = "bold";
 title.style.color = "green";
 
-const items = document.getElementsByClassName("list-group-item");
-// console.log(items.style);
-items[0].style.fontWeight = "bold";
-items[1].style.fontWeight = "bold";
-items[2].style.fontWeight = "bold";
-items[3].style.fontWeight = "bold";
-items[2].style.backgroundColor = "green";
+// const items = document.getElementsByClassName("list-group-item");
+// // console.log(items.style);
+
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
+// items[2].style.backgroundColor = "green";
+
+const li = document.getElementsByTagName("li");
+// console.log(li);
+for (let i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
+  li[i].style.backgroundColor = "skyblue";
+}
