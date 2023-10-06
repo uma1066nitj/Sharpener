72ee5d68494b2d7aeed6a49ee9496cd01a1d142a
Uma Shankar <umashankarjha1066@gmail.com>
2023-10-06 07:10:59 +0530

    queryselector added

--- dom.js
+++ dom.js
@@ -16,9 +16,25 @@ title.style.color = "green";
 // }
 // items[2].style.backgroundColor = "green";
 
-const li = document.getElementsByTagName("li");
-// console.log(li);
-for (let i = 0; i < li.length; i++) {
-  li[i].style.fontWeight = "bold";
-  li[i].style.backgroundColor = "skyblue";
+// const li = document.getElementsByTagName("li");
+// // console.log(li);
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.fontWeight = "bold";
+//   li[i].style.backgroundColor = "skyblue";
+// }
+
+// const secondItem = document.querySelector(".list-group-item:nth-child(2)");
+// console.log(items);
+// secondItem.style.backgroundColor = "green";
+// const thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+// console.log(items);
+// thirdItem.style.display = "none";
+
+const odd = document.querySelectorAll("li:nth-child(odd)");
+const even = document.querySelectorAll("li:nth-child(even)");
+// console.log(list);
+even[0].style.color = "green";
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "green";
+  even[i].style.backgroundColor = "grey";
 }
