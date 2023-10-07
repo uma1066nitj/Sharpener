dd051bf73d4c3fe5e7c5bfb0f77693a04b8aec4d
Uma Shankar <umashankarjha1066@gmail.com>
2023-10-07 15:20:53 +0530

    filter added

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ form.addEventListener("submit", addItem);
 //remove item
 itemList.addEventListener("click", removeItem);
 
+//Filter Item
+filter.addEventListener("keyup", filterItems);
+
 //addItem Function
 function addItem(e) {
   e.preventDefault();
 
-  var newItem = document.getElementById("item").value;
+  var newItem =
+    document.getElementById("item").value +
+    " " +
+    document.getElementById("description").value;
 
   //create new Element li
   var li = document.createElement("li");
@@ -26,8 +32,13 @@ function addItem(e) {
   //add className
   deleteBtn.className = "btn btn-danger btn-sm float-right delete";
   deleteBtn.appendChild(document.createTextNode("X"));
-
   li.appendChild(deleteBtn);
+
+  //add edit button
+  var editBtn = document.createElement("button");
+  editBtn.className = "btn btn-sm float-right edit";
+  editBtn.appendChild(document.createTextNode("Edit"));
+  li.appendChild(editBtn);
 
   itemList.appendChild(li);
 }
@@ -40,4 +51,21 @@ function removeItem(e) {
       itemList.removeChild(li);
     }
   }
+}
+
+function filterItems(e) {
+  //convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  //get list
+  var items = itemList.getElementsByTagName("li");
+  //   console.log(items);
+  //convert to array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
