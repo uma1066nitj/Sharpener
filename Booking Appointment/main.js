7ae3e740b7f032cc7322e3144a8f46eaea678456
Uma Shankar <umashankarjha1066@gmail.com>
2023-10-07 23:11:44 +0530

    store object

--- Booking Appointment/main.js
+++ Booking Appointment/main.js
@@ -1,29 +1,38 @@
-const myForm = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const msg = document.querySelector(".msg");
-const userList = document.querySelector("#users");
+// const myForm = document.querySelector("#my-form");
+// const nameInput = document.querySelector("#name");
+// const emailInput = document.querySelector("#email");
+// const msg = document.querySelector(".msg");
+// const userList = document.querySelector("#users");
 
-myForm.addEventListener("submit", onSubmit);
+// myForm.addEventListener("submit", onSubmit);
 
-function onSubmit(e) {
-  e.preventDefault();
+// function onSubmit(e) {
+//   e.preventDefault();
 
-  if (nameInput.value === "" || emailInput.value === " ") {
-    msg.classList.add("error");
-    msg.innerHTML = "Please enter all values";
+//   if (nameInput.value === "" || emailInput.value === " ") {
+//     msg.classList.add("error");
+//     msg.innerHTML = "Please enter all values";
 
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
-    );
-    userList.appendChild(li);
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
+//     const li = document.createElement("li");
+//     li.appendChild(
+//       document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+//     );
+//     userList.appendChild(li);
 
-    localStorage.setItem(nameInput.value, emailInput.value);
-    console.log(localStorage.getItem(nameInput.value));
-    nameInput.value = "";
-    emailInput.value = "";
-  }
-}
+//     localStorage.setItem(nameInput.value, emailInput.value);
+//     console.log(localStorage.getItem(nameInput.value));
+//     nameInput.value = "";
+//     emailInput.value = "";
+//   }
+// }
+
+//storing objects with local storage
+let myObj = { name: "Domenic", age: 56 };
+
+let myObj_seralized = JSON.stringify(myObj);
+localStorage.setItem("myObj", myObj_seralized);
+
+let myObj_deseralized = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deseralized);
