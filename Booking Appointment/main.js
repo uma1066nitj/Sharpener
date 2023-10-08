cf19eb31b138061ea1cfd40b5497aaf00701a7cc
Uma Shankar <umashankarjha1066@gmail.com>
2023-10-08 11:11:30 +0530

    edit button added

--- Booking Appointment/main.js
+++ Booking Appointment/main.js
@@ -22,8 +22,12 @@ function onSubmit(e) {
   } else {
     const li = document.createElement("li");
     const deleteBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
+
     deleteBtn.className = "delete";
+    editBtn.className = "edit";
     deleteBtn.appendChild(document.createTextNode("Delete"));
+    editBtn.appendChild(document.createTextNode("Edit"));
     li.appendChild(
       document.createTextNode(
         `Name : ${nameInput.value}, Email : ${emailInput.value}, Contact: ${contactInput.value}`
@@ -37,7 +41,9 @@ function onSubmit(e) {
       }
     }
     var user = new User(nameInput.value, emailInput.value, contactInput.value);
+
     li.appendChild(deleteBtn);
+    li.appendChild(editBtn);
     userList.appendChild(li);
   }
   localStorage.setItem(user.email, JSON.stringify(user));
@@ -49,13 +55,25 @@ function onSubmit(e) {
   //delete elements After adding
   var items = userList.getElementsByTagName("li");
   Array.from(items).forEach(function (item) {
-    const btn = item.querySelector(".delete");
-    btn.addEventListener("click", removeItem);
+    const deleteButton = item.querySelector(".delete");
+    const editButton = item.querySelector(".edit");
+    deleteButton.addEventListener("click", removeItem);
+    editButton.addEventListener("click", editItem);
     function removeItem(e) {
       if (e.target.className == "delete") {
         var item = e.target.parentElement;
         userList.removeChild(item);
         localStorage.removeItem(user.email);
+      }
+    }
+    function editItem(e) {
+      if (e.target.className == "edit") {
+        var item = e.target.parentElement;
+        userList.removeChild(item);
+        localStorage.removeItem(user.email);
+        nameInput.value = user.name;
+        emailInput.value = user.email;
+        contactInput.value = user.contact;
       }
     }
   });
