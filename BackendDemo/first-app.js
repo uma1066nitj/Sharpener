ff413169c1cb1b04048080f0397ebb2d7b72500f
Uma Shankar <umashankarjha1066@gmail.com>
2023-12-05 08:04:10 +0530

    Added Routing Request

--- BackendDemo/first-app.js
+++ BackendDemo/first-app.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 4000;
+const personName = "Uma Shankar";
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Home Page</title></head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>About Page</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Node.js Project</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/ by ${personName}`);
+});
