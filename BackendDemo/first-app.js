88afbf285833c688eb730b76eb91215a6fceaff7
Uma Shankar <umashankarjha1066@gmail.com>
2023-12-12 00:10:34 +0530

    express installed and added in this

--- BackendDemo/first-app.js
+++ BackendDemo/first-app.js
@@ -1,12 +1,31 @@
-const http = require("http");
-const routes = require("./routes");
+// const http = require("http");
+const express = require("express");
+const app = express();
+const value = 78;
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another Middleware");
+  // res.send("<h1>Hello</h1>");
+  res.send({ key1: value });
+  // next();
+});
+// const routes = require("./routes");
 const hostname = "127.0.0.1";
 const port = 4000;
 const personName = "Uma Shankar";
 
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
+// const server = http.createServer(routes.handler);
+// const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
+// console.log(routes.someText);
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/ by ${personName}`);
+// });
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/ by ${personName}`);
 });
