c2b6387eae0eba920e2ae94e02c69835a44e9542
Uma Shankar <umashankarjha1066@gmail.com>
2023-12-13 20:28:23 +0530

    admin and shop added

---
+++ BackendDemo/routes/admin.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/admin/add-product' method='POST'><input type='text' name='title' placeholder='Enter the name'><input type='text' name='size' placeholder='Enter the Size'><button type='submit'>Submit</button></form >"
+  );
+});
+
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+module.exports = router;
