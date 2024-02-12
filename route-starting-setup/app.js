64fccc4e4ef0a597dc21cc7888b9f534ec77641d
Uma Shankar <umashankarjha1066@gmail.com>
2024-02-12 08:16:06 +0530

    Sequelized added

--- route-starting-setup/app.js
+++ route-starting-setup/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,14 +12,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-db.execute("SELECT * FROM products")
-  .then((result) => {
-    console.log(result[0], result[1]);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -29,4 +21,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(4000);
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch((err) => console.log(err));
--- route-starting-setup/models/product.js
+++ route-starting-setup/models/product.js
@@ -1,37 +1,68 @@
-const db = require("../util/database");
+// const db = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
 
-  save() {
-    return db.execute(
-      "INSERT INTO products (title,price,description, imageUrl) VALUES (?,?,?,?)",
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-  }
+//   save() {
+//     return db.execute(
+//       "INSERT INTO products (title,price,description, imageUrl) VALUES (?,?,?,?)",
+//       [this.title, this.price, this.description, this.imageUrl]
+//     );
+//   }
 
-  // static deleteproductByid(id) {
-  //   return db.execute("Delete Table products where id = ?", [id]);
-  // }
-  static deleteproductByid(id) {
-    if (!id) {
-      return Promise.reject(new Error("Product ID is required."));
-    }
+//   // static deleteproductByid(id) {
+//   //   return db.execute("Delete Table products where id = ?", [id]);
+//   // }
+//   static deleteproductByid(id) {
+//     if (!id) {
+//       return Promise.reject(new Error("Product ID is required."));
+//     }
 
-    return db.execute("DELETE FROM products WHERE id = ?", [id]);
-  }
+//     return db.execute("DELETE FROM products WHERE id = ?", [id]);
+//   }
 
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
+//   static fetchAll() {
+//     return db.execute("SELECT * FROM products");
+//   }
 
-  static findbyId(id) {
-    return db.execute("SELECT * FROM products WHERE id = ?", [id]);
-  }
-};
+//   static findbyId(id) {
+//     return db.execute("SELECT * FROM products WHERE id = ?", [id]);
+//   }
+// };
+
+const Sequelize = require("sequelize");
+
+const sequelize = require("../util/database");
+
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
