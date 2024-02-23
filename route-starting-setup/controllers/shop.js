16ade3467a5b58eee5cb745f3a5b631fce22971c
Uma Shankar <umashankarjha1066@gmail.com>
2024-02-24 00:01:18 +0530

    Cart with many to many relationship

--- route-starting-setup/controllers/shop.js
+++ route-starting-setup/controllers/shop.js
@@ -55,12 +55,65 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findbyId(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
-};
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
 
+      let newQuantity = 1;
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        // return fetchedCart.addProduct(product, {
+        //   through: { quantity: newQuantity },
+        // });
+        return product;
+      }
+      return (
+        Product.findByPk(prodId)
+          // .then((product) => {
+          //   return fetchedCart.addProduct(product, {
+          //     through: { quantity: newQuantity },
+          //   });
+          // })
+          .catch((err) => console.log(err))
+      );
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
